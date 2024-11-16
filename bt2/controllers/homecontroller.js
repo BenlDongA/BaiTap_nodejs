4d5d0b756b66b4741ecb8369242fff374b137a90
BenlDongA <128448718+BenlDongA@users.noreply.github.com>
2024-11-16 09:05:14 +0700

    Delete bt2 directory

--- bt2/controllers/homecontroller.js
+++
@@ -1,9 +0,0 @@
-
-class HomeController{
-    static Home(req, res)  {
-        console.log(req.query)
-        res.render("Home", { title: "Home Page"});
-      }
-    
-}
-export default HomeController
